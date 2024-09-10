cf636d135ddece32131af6d7093f02aadd730940
Vladimir Petko <vladimir.petko@canonical.com>
2024-09-10 17:02:07 +1200

    feat: plugin skeleton.

---
+++ plugin/src/main/java/com/canonical/rockcraft/plugin/BuildRockcraftTask.java
@@ -0,0 +1,12 @@
+package com.canonical.rockcraft.plugin;
+
+import org.gradle.api.DefaultTask;
+import org.gradle.api.tasks.TaskAction;
+
+public class BuildRockcraftTask extends DefaultTask {
+
+    @TaskAction
+    public void buildROCK() {
+
+    }
+}
