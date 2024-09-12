2348b04c8bbd69d4bede81c2f60f1c0407e7bc90
Vladimir Petko <vladimir.petko@canonical.com>
2024-09-13 10:33:09 +1200

    feat: move rock into rock/ directory.
    Ensure that only output of the latest build
    remains in the build directory

--- rockcraft-plugin/src/main/java/com/canonical/rockcraft/plugin/BuildRockcraftTask.java
+++ rockcraft-plugin/src/main/java/com/canonical/rockcraft/plugin/BuildRockcraftTask.java
@@ -5,8 +5,11 @@ import org.gradle.api.tasks.TaskAction;
 
 import java.io.IOException;
 import java.io.UnsupportedEncodingException;
+import java.nio.file.Files;
 
 public class BuildRockcraftTask extends DefaultTask {
+
+    private static final String ROCK_DIR = "rock";
 
     @TaskAction
     public void packRock() throws IOException, InterruptedException {
@@ -18,5 +21,17 @@ public class BuildRockcraftTask extends DefaultTask {
         int result = process.waitFor();
         if (result != 0)
             throw new UnsupportedEncodingException("Failed to pack rock for "+ getProject().getName());
+
+        var rockDest = buildDir.dir(ROCK_DIR).get().getAsFile();
+        rockDest.mkdirs();
+        for (var f : rockDest.listFiles((dir,file) -> file.endsWith(".rock"))) {
+            f.delete();
+        }
+        // refresh rocks
+        for (var f : buildDir.getAsFile().get().listFiles((dir,file) -> file.endsWith(".rock"))) {
+            var source = f.toPath();
+            var dest = buildDir.dir(ROCK_DIR).get().getAsFile().toPath();
+            Files.move(source, dest.resolve(source.getFileName()));
+        }
     }
 }
