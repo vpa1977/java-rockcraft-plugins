c4f3bb92e6347785defff9fe056a6041b4140c17
Vladimir Petko <vladimir.petko@canonical.com>
2024-09-23 14:57:21 +1200

    chore: add personal plugin entrypoint for EA release

---
+++ rockcraft-plugin/src/main/java/io/github/vpa1977/rockcraft/plugin/RockcraftPlugin.java
@@ -0,0 +1,7 @@
+package io.github.vpa1977.rockcraft;
+
+/**
+ * An early access plugin is to be published using the personal namespace
+ */
+public class RockcraftPlugin extends com.canonical.rockcraft.plugin.RockcraftPlugin {
+}
