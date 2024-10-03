c00a8332d5169cb1c835ec47bfa3df05bd189c18
Vladimir Petko <vladimir.petko@canonical.com>
2024-10-03 13:55:35 +1300

    fix: RockSettingsFactory compile error
    fix wrong import
    fix output directory

--- rockcraft-gradle/src/main/java/com/canonical/rockcraft/gradle/RockSettingsFactory.java
+++ rockcraft-gradle/src/main/java/com/canonical/rockcraft/gradle/RockSettingsFactory.java
@@ -2,11 +2,12 @@ package com.canonical.rockcraft.gradle;
 
 import org.gradle.api.Project;
 
-import com.rockcraft.builder.RockProjectSettings;
+import com.canonical.rockcraft.builder.RockProjectSettings;
 
 public class RockSettingsFactory {
     public static final RockProjectSettings createRockProjectSettings(Project project) {
+        var output = project.getLayout().getBuildDirectory().getAsFile().get().toPath();
         return new RockProjectSettings("gradle", project.getName(), String.valueOf(project.getVersion()), project.getProjectDir().toPath(),
-            project.getLayout().getBuildDirectory());
+            output);
     }
 }
