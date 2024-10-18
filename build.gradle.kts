5697acc70224674a57952006b69034aeddf2c41b
Vladimir Petko <vladimir.petko@canonical.com>
2024-10-18 19:00:04 +1300

    chore: add github packages publication

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,19 @@
 allprojects {
     version = "0.2.1"
     group = "io.github.rockcrafters"
+    apply(plugin = "maven-publish")
+    plugins.withType<MavenPublishPlugin>().configureEach {
+        extensions.configure<PublishingExtension>() {
+            repositories {
+                maven {
+                    name = "GitHubPackages"
+                    url = uri(project.findProperty("gpr.repo") ?: "")
+                    credentials {
+                        username = project.findProperty("gpr.user") as String? ?: System.getenv("GHP_USERNAME")
+                        password = project.findProperty("gpr.key") as String? ?: System.getenv("GHP_TOKEN")
+                    }
+                }
+            }
+        }
+    }
 }
